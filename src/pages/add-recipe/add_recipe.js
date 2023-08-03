07074b0cbd84b21ead5056c40c40382c6549be3a
Maulana Ismail <maulanaismail3466@gmail.com>
2023-08-03 11:24:06 +0700

    [CHANGE] Recreate all views on add recipe page

--- src/pages/add-recipe/add_recipe.js
+++ src/pages/add-recipe/add_recipe.js
@@ -2,7 +2,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "../../styles/add-recipe/add_recipe.css";
-import Footer2 from "../../components/organisms/footer2";
+import Navbar from "../../components/organisms/navbar";
+import Footer from "../../components/organisms/footer";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 
@@ -86,193 +87,157 @@ function AddRecipe() {
 
   return (
     <div id="add-recipe">
-      <div className="container-fluid add-recipe p-0">
-        {/* <!-- NAVBAR --> */}
-        <nav
-          id="navbar"
-          className="container navbar navbar-expand-lg p-0 mt-4 mb-4"
-        >
-          <button
-            className="navbar-toggler navbar-sm-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div
-            className="collapse navbar-collapse list"
-            id="navbarSupportedContent"
-          >
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item me-5">
-                <Link
-                  to="/"
-                  className="nav-link active text-decoration-none"
-                  aria-current="page"
-                >
-                  Home
-                </Link>
-              </li>
-              <li className="nav-item me-5">
-                <Link
-                  to="/add-recipe"
-                  className="nav-link active fw-bold"
-                  aria-current="page"
-                >
-                  Add Recipe
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link
-                  to="/profile-my-recipe"
-                  className="nav-link active"
-                  aria-current="page"
-                >
-                  Profile
-                </Link>
-              </li>
-            </ul>
-          </div>
-        </nav>
-        {/* <!-- END OF NAVBAR --> */}
+      <div className="container-fluid addRecipe p-0">
+        {/* NAVBAR */}
+        <Navbar />
+        {/* END OF NAVBAR */}
 
-        {/* <!-- FORM ADD RECIPE --> */}
-        <section id="form" className="container">
-          {/* <!-- form image --> */}
-          <div className="row mb-4">
-            <div className="col-12">
-              <div className="d-flex justify-content-center">
-                <div className="btn btn-rounded">
-                  <label
-                    className="form-label form-label m-1 form-image border bg-body-tertiary d-flex justify-content-center align-items-center shadow-sm"
-                    for="customFile1"
-                    style={{ backgroundImage: `url(${uploadImg})` }}
-                  >
-                    Choose file
-                  </label>
-                  <input
-                    type="file"
-                    className="form-control d-none"
-                    id="customFile1"
-                    accept="image/*"
-                    onChange={(e) => {
-                      setUploadImg(URL.createObjectURL(e.target.files[0]));
-                      setPicture(e.target.files[0]);
-                      // URL.createObjectURL(e.target.files[0]);
-                    }}
-                  />
-                </div>
+        <section className="container form">
+          {/* TITLE PAGE */}
+          <div className="row titlePage">
+            <div className="col-12 d-flex justify-content-center">
+              <div>
+                <h1>Add Your Recipe</h1>
               </div>
             </div>
           </div>
-          <div className="row">
+          {/* END OF TITLE PAGE */}
+
+          {/* ADD IMAGE FORM */}
+          <section className="row addImage">
             <div className="col-12 d-flex justify-content-center">
-              {uploadSuccess ? (
-                <div
-                  class="alert alert-success"
-                  role="alert"
-                  style={{
-                    fontSize: "16px",
-                    border: "0",
-                    borderRadius: "8px",
-                    padding: "13px 0 0 0",
-                    width: "400px",
+              <label className="custum-file-upload" for="file">
+                {uploadImg === null ? (
+                  <>
+                    <div className="icon">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill=""
+                        viewBox="0 0 24 24"
+                      >
+                        <g stroke-width="0" id="SVGRepo_bgCarrier"></g>
+                        <g
+                          stroke-linejoin="round"
+                          stroke-linecap="round"
+                          id="SVGRepo_tracerCarrier"
+                        ></g>
+                        <g id="SVGRepo_iconCarrier">
+                          {" "}
+                          <path
+                            fill=""
+                            d="M10 1C9.73478 1 9.48043 1.10536 9.29289 1.29289L3.29289 7.29289C3.10536 7.48043 3 7.73478 3 8V20C3 21.6569 4.34315 23 6 23H7C7.55228 23 8 22.5523 8 22C8 21.4477 7.55228 21 7 21H6C5.44772 21 5 20.5523 5 20V9H10C10.5523 9 11 8.55228 11 8V3H18C18.5523 3 19 3.44772 19 4V9C19 9.55228 19.4477 10 20 10C20.5523 10 21 9.55228 21 9V4C21 2.34315 19.6569 1 18 1H10ZM9 7H6.41421L9 4.41421V7ZM14 15.5C14 14.1193 15.1193 13 16.5 13C17.8807 13 19 14.1193 19 15.5V16V17H20C21.1046 17 22 17.8954 22 19C22 20.1046 21.1046 21 20 21H13C11.8954 21 11 20.1046 11 19C11 17.8954 11.8954 17 13 17H14V16V15.5ZM16.5 11C14.142 11 12.2076 12.8136 12.0156 15.122C10.2825 15.5606 9 17.1305 9 19C9 21.2091 10.7909 23 13 23H20C22.2091 23 24 21.2091 24 19C24 17.1305 22.7175 15.5606 20.9844 15.122C20.7924 12.8136 18.858 11 16.5 11Z"
+                            clip-rule="evenodd"
+                            fill-rule="evenodd"
+                          ></path>{" "}
+                        </g>
+                      </svg>
+                    </div>
+                    <div className="text">
+                      <span>Click to upload a image of your recipe</span>
+                    </div>
+                  </>
+                ) : (
+                  <>
+                    <div
+                      className="imageRecipe"
+                      style={{ backgroundImage: `url(${uploadImg})` }}
+                    ></div>
+                  </>
+                )}
+                <input
+                  type="file"
+                  id="file"
+                  onChange={(e) => {
+                    setUploadImg(URL.createObjectURL(e.target.files[0]));
+                    setPicture(e.target.files[0]);
+                    // URL.createObjectURL(e.target.files[0]);
                   }}
-                >
-                  <p className="text-center">Recipe added successfully</p>
-                </div>
-              ) : null}
-
-              {uploadError ? (
-                <div
-                  class="alert alert-danger"
-                  role="alert"
-                  style={{
-                    fontSize: "16px",
-                    border: "0",
-                    borderRadius: "8px",
-                    padding: "13px 0 0 0",
-                    width: "400px",
-                  }}
-                >
-                  <p className="text-center">{errorMSg}</p>
-                </div>
-              ) : null}
+                />
+              </label>
             </div>
-          </div>
+          </section>
+          {/* END OF ADD IMAGE FORM */}
 
-          {/* <!-- form title --> */}
-          <div className="row mb-4">
+          {/* ADD RECIPE TITLE */}
+          <section className="row addRecipeTitle">
             <div className="col-12 d-flex justify-content-center">
               <input
                 type="text"
-                className="form-control form-title ps-4 py-2 bg-body-tertiary shadow-sm"
-                placeholder="Title"
+                className="form-control form-title bg-body-tertiary"
+                placeholder="Recipe Title"
                 onChange={(e) => {
                   setTitle(e.target.value);
                 }}
               />
             </div>
-          </div>
+          </section>
+          {/* END OF ADD RECIPE TITLE */}
 
-          {/* <!-- form ingredient --> */}
-          <div className="row">
+          {/* ADD INGREDIENT */}
+          <section className="row addIngredients">
             <div className="col-12 d-flex justify-content-center">
-              <div className="form-ingredient">
-                <div className="mb-3">
-                  <textarea
-                    className="form-control border-2 form-comment bg-body-tertiary shadow-sm"
-                    rows="3"
-                    placeholder="ingredients"
-                    onChange={(e) => {
-                      setIngredients(e.target.value);
-                    }}
-                  ></textarea>
-                </div>
-              </div>
+              <textarea
+                type="text"
+                className="form-control"
+                placeholder="Ingredients"
+                onChange={(e) => {
+                  setIngredients(e.target.value);
+                }}
+              />
             </div>
-          </div>
+          </section>
+          {/* END OF ADD INGREDIENT */}
 
-          {/* <!-- form video --> */}
-          <div className="row mb-4">
+          {/* ADD A RECIPE VIDEO STEP LINK */}
+          <section className="row addLinkVideo">
             <div className="col-12 d-flex justify-content-center">
               <input
                 type="text"
-                className="form-control form-title ps-4 py-2 bg-body-tertiary shadow-sm"
-                placeholder="Video"
+                className="form-control"
+                placeholder="Step video link (Example: https://www.youtube.com/....)"
                 onChange={(e) => {
                   setVideo(e.target.value);
                 }}
               />
             </div>
-          </div>
+          </section>
+          {/* END OF ADD A RECIPE VIDEO STEP LINK */}
 
-          <div className="row">
+          {/* BUTTON UPLOAD RECIPE */}
+          <section className="row btnUpload">
             <div className="col-12 d-flex justify-content-center">
-              <div className="text-decoration-none text-light">
-                <button
-                  type="button"
-                  className="btn btn-warning text-light post"
-                  onClick={() => {
-                    sendData();
-                  }}
-                  disabled={loading}
-                >
-                  {loading ? "Upload.." : "Post"}
-                </button>
-              </div>
+              <button
+                type="button"
+                className="btn btn-warning"
+                onClick={() => {
+                  sendData();
+                }}
+                disabled={loading}
+              >
+                {loading ? "Upload.." : "Post"}
+              </button>
             </div>
-          </div>
+          </section>
+          {/* END OF BUTTON UPLOAD RECIPE */}
         </section>
-        {/* <!-- END OF FORM ADD RECIPE --> */}
 
-        {/* <!-- FOOTER --> */}
-        <Footer2 />
-        {/* <!-- END OF FOOTER --> */}
+        {/* FOOTER */}
+        <Footer />
+        {/* END OF FOOTER */}
+
+        {/* STYLE FOR SCROLL BAR BROWSER */}
+        <style>
+          {`
+            ::-webkit-scrollbar {
+              width: 0em;
+              height: 0.5em;
+            }
+            ::-webkit-scrollbar-thumb {
+              background-color: rgba(0, 0, 0, 0.2);
+            }
+          `}
+        </style>
+        {/* STYLE FOR SCROLL BAR BROWSER */}
       </div>
     </div>
   );
