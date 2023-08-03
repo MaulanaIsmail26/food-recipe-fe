11603002de671cf3fba84ef424e8f64e109135d8
Maulana Ismail <maulanaismail3466@gmail.com>
2023-08-03 20:11:39 +0700

    [CHANGE] Recreate all views on Profile user page

--- src/pages/profile/profile_my_recipe.js
+++ src/pages/profile/profile_my_recipe.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "../../styles/profile/profile.css";
+import Navbar from "../../components/organisms/navbar";
 import Footer from "../../components/organisms/footer";
 import { useNavigate } from "react-router-dom";
-import RecipeCardV2 from "../../components/molecules/RecipeCardV2";
 import Loading from "../../components/molecules/loading";
 import axios from "axios";
+
+// IMPORT BY MATERIAL UI
+import LogoutIcon from "@mui/icons-material/Logout";
 
 function ProfileMyrecipe() {
   const navigate = useNavigate();
@@ -47,152 +50,107 @@ function ProfileMyrecipe() {
 
   return (
     <div id="profile-page">
-      <div className="container-fluid p-0">
-        {/* <!-- NAVBAR --> */}
-        <nav
-          id="navbar"
-          className="container navbar navbar-expand-lg p-0 mt-4 mb-4 fw-bold"
-        >
-          <button
-            className="navbar-toggler navbar-sm-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div
-            className="collapse navbar-collapse list"
-            id="navbarSupportedContent"
-          >
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item me-5">
-                <Link
-                  to="/"
-                  className="nav-link fw-normal active"
-                  aria-current="page"
-                >
-                  Home
-                </Link>
-              </li>
-              <li className="nav-item me-5">
-                <Link
-                  to="/add-recipe"
-                  className="nav-link fw-normal active"
-                  aria-current="page"
-                >
-                  Add Recipe
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link
-                  to="/profile-my-recipe"
-                  className="nav-link active"
-                  aria-current="page"
-                >
-                  Profile
-                </Link>
-              </li>
-            </ul>
-          </div>
-        </nav>
-        {/* <!-- END OF NAVBAR --> */}
+      <div className="container-fluid mainContainer p-0">
+        {/* NAVBAR */}
+        <Navbar />
+        {/* END OF NAVBAR */}
 
-        {/* <!-- PHOTO PROFILE & NAME --> */}
-        <section id="profile" className="container mt-1">
-          <div className="row text-center photo">
+        <section className="container content">
+          {/* PROFILE SECTION */}
+          <div className="row profileSection">
             <div className="col-12">
-              <img
-                src={profile?.photo}
-                alt="placeholder"
-                className="photo"
-              />
-            </div>
-          </div>
-          <div className="row text-center mt-3">
-            <div className="col-12">
-              <h6>{profile?.name}</h6>
-              <Link to="/logout">
-                <button type="button" class="btn btn-secondary btn-sm">
-                  Log Out
+              {/* PROFILE PICTURE */}
+              <div className="profilePicture">
+                <img
+                  src={require("../../asset/profilePicture.jpg")}
+                  class="mx-auto d-block"
+                  alt="profile_picture"
+                ></img>
+              </div>
+              {/* USERNAME */}
+              <div className="username d-flex justify-content-center">
+                <h4>Maulana Ismail</h4>
+              </div>
+              {/* BUTTON LOGOUT */}
+              <div className="btnLogout d-flex justify-content-center">
+                <button type="button" class="btn">
+                  <LogoutIcon className="iconLogout" /> Logout
                 </button>
-              </Link>
+              </div>
             </div>
           </div>
-        </section>
-        {/* <!-- END OF PHOTO PROFILE & NAME --> */}
+          {/* END OF PROFILE SECTION */}
 
-        {/* <!-- RECIPE --> */}
-        <section id="recipe" className="container mt-3">
-          <h3
-            className="pb-3 border-bottom border-dark border-5 mb-4"
-            style={{ width: "275px", fontSize: "35px" }}
-          >
-            Popular For You !
-          </h3>
-
-          {isLoading ? <Loading /> : null}
-
-          {recipe.length === 0 && !isLoading ? (
-            <h2 className="d-flex justify-content-center">Recipe not found</h2>
-          ) : null}
-
-          <div className="row align-items-md-center">
-            {recipe.slice(0, 3).map((item, key) => {
-              return (
-                <div className="col-3 me-1 mt-4" key={key}>
-                  <RecipeCardV2
-                    image={item?.picture}
-                    name={item?.title}
-                    url={item?.title}
-                    slug={item?.slug}
-                  />
+          {/* POPULAR FOR YOU */}
+          <div className="row forYou">
+            <div className="col-12">
+              {/* TITLE SECTION */}
+              <div className="row title">
+                <div className="col-12 ps-sm-4">
+                  <div>
+                    <h1>Popular For You!</h1>
+                  </div>
                 </div>
-              );
-            })}
-          </div>
+              </div>
 
-          {/* <div className="row">
-            <div className="col-3">
-              <div className="card" style={{ width: "16rem" }}>
-                <img
-                  src="../asset/food-img12.jpg"
-                  className="card-img-top"
-                  alt="placeholder"
-                />
-                <div className="card-body">
-                  <h5 className="card-title">Bomb Chicken</h5>
-                  <Link to="/detail-recipe" className="btn btn-primary">
-                    read recipe
-                  </Link>
+              {/* RECIPES SECTION */}
+              <div className="row recipes">
+                <div
+                  className="col-12 d-flex justify-content-evenly"
+                >
+                  <div className="cardRecipe position-relative">
+                    <img
+                      src="../../asset/food-img4.jpg"
+                      alt="iconApp"
+                      className="imgRecipe"
+                    />
+                    <p className="position-absolute">
+                      Maulana Ismail Maulana Ismail
+                    </p>
+                  </div>
+                  <div className="cardRecipe position-relative">
+                    <img
+                      src="../../asset/food-img-13.jpg"
+                      alt="iconApp"
+                      className="imgRecipe"
+                    />
+                    <p className="position-absolute">
+                      Maulana Ismail Maulana Ismail
+                    </p>
+                  </div>
+                  <div className="cardRecipe position-relative">
+                    <img
+                      src="../../asset/food-img6.jpg"
+                      alt="iconApp"
+                      className="imgRecipe"
+                    />
+                    <p className="position-absolute">
+                      Maulana Ismail Maulana Ismail
+                    </p>
+                  </div>
                 </div>
               </div>
             </div>
-            <div className="col-3">
-              <div className="card" style={{ width: "16rem" }}>
-                <img
-                  src="../asset/food-img-13.jpg"
-                  className="card-img-top"
-                  alt="placeholder"
-                />
-                <div className="card-body">
-                  <h5 className="card-title">Bananas Pancake</h5>
-                  <Link to="/detail-recipe" className="btn btn-primary">
-                    read recipe
-                  </Link>
-                </div>
-              </div>
-            </div>
-          </div> */}
+          </div>
+          {/* END OF POPULAR FOR YOU */}
         </section>
-        {/* <!-- END OF RECIPE --> */}
 
-        {/* <!-- FOOTER --> */}
+        {/* FOOTER */}
         <Footer />
-        {/* <!-- END OF FOOTER --> */}
+        {/* END OF FOOTER */}
+
+        {/* STYLE FOR SCROLL BAR BROWSER */}
+        <style>
+          {`
+            ::-webkit-scrollbar {
+              width: 0em;
+              height: 0.5em;
+            }
+            ::-webkit-scrollbar-thumb {
+              background-color: rgba(0, 0, 0, 0.2);
+            }
+          `}
+        </style>
       </div>
     </div>
   );
